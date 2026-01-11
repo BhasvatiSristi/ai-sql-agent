ea54adbc5b630544a954d36e7f18e5c9b623eba5
Bhasvati Sristi <bhasvati2106@gmail.com>
2026-01-11 08:27:49 +0530

    seventh commit

--- setup_db.py
+++ setup_db.py
@@ -9,14 +9,23 @@ if os.path.exists("olist.db"):
 print("Building database...")
 
 files = {
-    "olist_orders_dataset": "https://raw.githubusercontent.com/olistbr/brazilian-ecommerce/master/olist_orders_dataset.csv",
-    "olist_customers_dataset": "https://raw.githubusercontent.com/olistbr/brazilian-ecommerce/master/olist_customers_dataset.csv",
-    "olist_order_items_dataset": "https://raw.githubusercontent.com/olistbr/brazilian-ecommerce/master/olist_order_items_dataset.csv",
-    "olist_products_dataset": "https://raw.githubusercontent.com/olistbr/brazilian-ecommerce/master/olist_products_dataset.csv",
-    "olist_order_payments_dataset": "https://raw.githubusercontent.com/olistbr/brazilian-ecommerce/master/olist_order_payments_dataset.csv",
-    "olist_order_reviews_dataset": "https://raw.githubusercontent.com/olistbr/brazilian-ecommerce/master/olist_order_reviews_dataset.csv",
-    "olist_sellers_dataset": "https://raw.githubusercontent.com/olistbr/brazilian-ecommerce/master/olist_sellers_dataset.csv",
+    "olist_orders_dataset":
+        "https://raw.githubusercontent.com/andrevcmelo/TCC_Andre_Melo_Pos-CESAR_SCHOOL_AED/main/olist_orders_dataset.csv",
+    "olist_products_dataset":
+        "https://raw.githubusercontent.com/andrevcmelo/TCC_Andre_Melo_Pos-CESAR_SCHOOL_AED/main/olist_products_dataset.csv",
+    "olist_order_items_dataset":
+        "https://raw.githubusercontent.com/andrevcmelo/TCC_Andre_Melo_Pos-CESAR_SCHOOL_AED/main/olist_order_items_dataset.csv",
+    "olist_order_payments_dataset":
+        "https://raw.githubusercontent.com/andrevcmelo/TCC_Andre_Melo_Pos-CESAR_SCHOOL_AED/main/olist_order_payments_dataset.csv",
+    "olist_order_reviews_dataset":
+        "https://raw.githubusercontent.com/andrevcmelo/TCC_Andre_Melo_Pos-CESAR_SCHOOL_AED/main/olist_order_reviews_dataset.csv",
+    "olist_customers_dataset":
+        "https://raw.githubusercontent.com/andrevcmelo/TCC_Andre_Melo_Pos-CESAR_SCHOOL_AED/main/olist_customers_dataset.csv",
+    "olist_sellers_dataset":
+        "https://raw.githubusercontent.com/andrevcmelo/TCC_Andre_Melo_Pos-CESAR_SCHOOL_AED/main/olist_sellers_dataset.csv",
 }
+
+
 
 conn = sqlite3.connect("olist.db")
 
